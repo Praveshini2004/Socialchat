bf8515440ad6af1628a0afe738ae6991493a5ec8
Praveshini2004 <146466699+Praveshini2004@users.noreply.github.com>
2024-04-21 20:25:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,15 @@ app.use('/images', express.static('images'));
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://socialchat-mern.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+            
+            
+));
 mongoose.set('strictQuery', false); 
 dotenv.config();
 mongoose.connect(
