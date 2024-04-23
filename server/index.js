a0f73b0e125cd30e891f96923d95d876d1d43c78
Praveshini2004 <146466699+Praveshini2004@users.noreply.github.com>
2024-04-23 08:08:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors(
   {
-  origin: ["https://socialchatapp.vercel.app/"],
+  origin: ["https://socialchat-mern.vercel.app/"],
   methods: ["POST", "GET"],
   credentials: true
 }
